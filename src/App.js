d856a137a6daf3899d612c3be78f354a3c3a2a56
黄周慧 <hzhui5991@163.com>
2019-09-05 18:00:25 +0800

    图片选择组件

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
+
 import logo from './logo.svg';
+import React, { Component,useState } from 'react';
+import {Button, Checkbox, Col} from 'antd';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const PictureSelect = (props) => {
+  const {pictures, value, onChange} = props;
+    return(
+      <Checkbox.Group onChange={onChange} value = {value} >
+        { pictures.map(item =>
+          <div
+               className='div_check' 
+               key={item.id}>
+            <Checkbox value={item.id} className='checkbox'></Checkbox>
+            <div style={{
+              height:'130px',
+              overflow:'hidden',
+              background:`url(${item.url}) no-repeat center`,
+              backgroundSize: 'cover',
+              width:'130px',
+              margin: '0 auto',
+            }}
+             >
+            </div>
+            <p>
+              {item.name}
+            </p>
+          </div>
+        )}
+    </Checkbox.Group>
+)};
+
+
+class Picture extends Component {
+  constructor(props){
+    super(props);
+  }
+  state ={
+    allChecked:false,
+    value:[],
+  }
+  setValue(value){
+    console.log(value);
+    this.setState({value:value,allChecked:value.length===3});//总数
+  }
+  handleCheckAll=(e)=> {
+    const checked = e.target.checked;
+    this.setState({ allChecked: checked });
+    if(checked){
+      this.setState({ value: ['1','2','3'] });//全部
+    }else{
+      this.setState({ value: [] });
+    }
+  }
+  render(){
+    const state = this.state;
+    // const [value, setValue] = React.useState(['1']);
+    const pictures = [
+      {
+        id: '1',
+        name: 'foo',
+        url: 'https://gw.alipayobjects.com/mdn/rms_d212b7/afts/img/A*LlfeSa8N0WgAAAAAAAAAAABkARQnAQ'
+      },
+      {
+        id: '2',
+        name: 'foo',
+        url: 'https://gw.alipayobjects.com/mdn/rms_d212b7/afts/img/A*LlfeSa8N0WgAAAAAAAAAAABkARQnAQ'
+      },
+      {
+        id: '3',
+        name: 'foo',
+        url: 'https://gw.alipayobjects.com/mdn/rms_d212b7/afts/img/A*LlfeSa8N0WgAAAAAAAAAAABkARQnAQ'
+      },
+    ]; 
+
+    return (
+      <div className="App">
+        <div>
+          <Checkbox
+              onChange={this.handleCheckAll} 
+              checked={this.state.allChecked}
+          >
+            已选中{this.state.value.length}个文件
+          </Checkbox>
+        </div>
+        <PictureSelect pictures={pictures} value={state.value} onChange={(value) => this.setValue(value)} />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Picture;
