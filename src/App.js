31cfc6699b6d911fd7ee6ba1a185b1c2b884b3ef
黄周慧 <hzhui5991@163.com>
2019-09-05 18:17:22 +0800

    图片选择组件

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
-
-import logo from './logo.svg';
 import React, { Component,useState } from 'react';
 import {Button, Checkbox, Col} from 'antd';
 import './App.css';
 
 const PictureSelect = (props) => {
-  const {pictures, value, onChange} = props;
+  const {pictures, value, onChange, onCheckAll} = props;
     return(
-      <Checkbox.Group onChange={onChange} value = {value} >
-        { pictures.map(item =>
-          <div
-               className='div_check' 
-               key={item.id}>
-            <Checkbox value={item.id} className='checkbox'></Checkbox>
-            <div style={{
-              height:'130px',
-              overflow:'hidden',
-              background:`url(${item.url}) no-repeat center`,
-              backgroundSize: 'cover',
-              width:'130px',
-              margin: '0 auto',
-            }}
-             >
-            </div>
-            <p>
-              {item.name}
-            </p>
-          </div>
-        )}
-    </Checkbox.Group>
+      <div className='App'>
+          <Checkbox
+              onChange={onCheckAll} 
+              checked={value.length===3}
+          >
+            已选中{value.length}个文件
+          </Checkbox>
+          <div>
+          <Checkbox.Group onChange={onChange} value = {value} >
+
+            { pictures.map(item =>
+              <div
+                   className='div_check' 
+                   key={item.id}>
+                <Checkbox value={item.id} className='checkbox'></Checkbox>
+                <div style={{
+                  height:'130px',
+                  overflow:'hidden',
+                  background:`url(${item.url}) no-repeat center`,
+                  backgroundSize: 'cover',
+                  width:'130px',
+                  margin: '0 auto',
+                }}
+                 >
+                </div>
+                <p>
+                  {item.name}
+                </p>
+              </div>
+            )}
+        </Checkbox.Group></div>
+        </div>
 )};
 
 
@@ -37,16 +45,13 @@ class Picture extends Component {
     super(props);
   }
   state ={
-    allChecked:false,
     value:[],
   }
   setValue(value){
-    console.log(value);
     this.setState({value:value,allChecked:value.length===3});//总数
   }
-  handleCheckAll=(e)=> {
+  onCheckAll=(e)=> {
     const checked = e.target.checked;
-    this.setState({ allChecked: checked });
     if(checked){
       this.setState({ value: ['1','2','3'] });//全部
     }else{
@@ -55,7 +60,6 @@ class Picture extends Component {
   }
   render(){
     const state = this.state;
-    // const [value, setValue] = React.useState(['1']);
     const pictures = [
       {
         id: '1',
@@ -75,17 +79,7 @@ class Picture extends Component {
     ]; 
 
     return (
-      <div className="App">
-        <div>
-          <Checkbox
-              onChange={this.handleCheckAll} 
-              checked={this.state.allChecked}
-          >
-            已选中{this.state.value.length}个文件
-          </Checkbox>
-        </div>
-        <PictureSelect pictures={pictures} value={state.value} onChange={(value) => this.setValue(value)} />
-      </div>
+        <PictureSelect pictures={pictures} value={state.value} onChange={(value) => this.setValue(value)}  onCheckAll={this.onCheckAll} />
     );
   }
 }
